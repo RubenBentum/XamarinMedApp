d0dec067728439fa9dc5da1f2b1216f67014801d
Unknown <r.bentum23031994@hotmail.com>
2018-05-23 12:00:17 +0200

    Initial commit

---
+++ XamarinApp/XamarinApp/ListViewAdapter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinApp
+{
+    class ListViewAdapter : BaseAdapter<string>
+    {
+        private List<string> mItems;
+        private Context mContext;
+        public ListViewAdapter(Context context, List<string> items)
+        {
+            mItems = items;
+            mContext = context;
+        }
+
+        // overrides inherited objects that are not used
+        public override int Count { get { return mItems.Count; } }
+        public override long GetItemId(int position) { return position;}
+        public override string this[int position] { get { return mItems[position]; } }
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            if (row == null)
+            {
+                row = LayoutInflater.From(mContext).Inflate(Resource.Layout.layoutScreen1, null, false);
+            }
+
+            TextView textScreen1 = row.FindViewById<TextView>(Resource.Id.textScreen1);
+            textScreen1.Text = mItems[position];
+
+            return row;
+
+        }
+        
+    }
+}
---
+++ XamarinApp/XamarinApp/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Views;
+
+
+
+namespace XamarinApp
+{
+    [Activity(Label = "XamarinApp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        private List<string> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mListView = FindViewById<ListView>(Resource.Id.listView1);
+
+            mItems = new List<string>();
+            mItems.Add("Button 1");
+            mItems.Add("Button 2");
+            mItems.Add("Button 3");
+
+            ListViewAdapter adapter = new ListViewAdapter(this, mItems);
+                        
+            mListView.Adapter = adapter;
+
+            
+
+        }
+    }
+}
+
